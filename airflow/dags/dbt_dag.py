aaec04a0dce9206fe06459245e0765cef556f30b
sais07 <sai@cbase.ai>
2025-03-06 00:16:48 -0500

    fixed issues with Dbt

--- airflow/dags/dbt_dag.py
+++ airflow/dags/dbt_dag.py
@@ -12,8 +12,8 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-dbt_snowflake_dag = DAG(
-    dag_id="dbt_snowflake_dag",
+dbt_dag = DAG(
+    dag_id="dbt_dag",
     default_args=default_args,
     description="Run dbt transformations in Snowflake",
     schedule_interval=None,
@@ -31,28 +31,28 @@ init_database = SnowflakeOperator(
     CREATE SCHEMA IF NOT EXISTS FINANCIAL_SCHEMA;
     USE SCHEMA FINANCIAL_SCHEMA;
     """,
-    dag=dbt_snowflake_dag,
+    dag=dbt_dag,
 )
 
 # Step 2: Debug dbt Configuration
 # dbt_debug = BashOperator(
 #     task_id="dbt_debug",
 #     bash_command="dbt debug --profiles-dir /opt/airflow/dags/data_validate --project-dir /opt/airflow/dags/data_validate",
-#     dag=dbt_snowflake_dag,
+#     dag=dbt_dag,
 # )
 
 # Step 3: Run dbt Models
 run_dbt_models = BashOperator(
     task_id="run_dbt_models",
     bash_command="set -e; dbt run --profiles-dir /opt/airflow/dags/ --project-dir /opt/airflow/dags/ --target dev",
-    dag=dbt_snowflake_dag,
+    dag=dbt_dag,
 )
 
 # Step 4: Test dbt Models
 test_dbt_models = BashOperator(
     task_id="test_dbt_models",
     bash_command="set -e; dbt test --profiles-dir /opt/airflow/dags/ --project-dir /opt/airflow/dags/ --target dev",
-    dag=dbt_snowflake_dag,
+    dag=dbt_dag,
 )
 
 # Define Task Dependencies
